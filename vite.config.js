63c941bceb099d18c5c4affc7183530903d0c7b2
M Mudasir Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-03-18 12:34:33 +0500

    add Vercel configuration files and update build settings in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000
+  },
   base: process.env.VITE_BASE_PATH || "/mmk-digital",
 })
+
+
