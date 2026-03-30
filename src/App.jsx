9fc025991d5420394af9f99fd7f0666843da47bb
M Mudasir Iqbal <mr.mudasiriqbal.developer@gmail.com>
2026-03-30 11:10:21 +0500

    Update Analytics import from '@vercel/analytics/next' to '@vercel/analytics/react'

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 import "./index.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
